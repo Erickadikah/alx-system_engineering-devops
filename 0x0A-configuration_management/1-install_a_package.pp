47f3e5803e98b6d432266b1c2eaea7aa0c681f88
erickadikah <erickadikah2030@gmail.com>
2022-12-09 18:26:26 +0300

    bugged

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # using puppet to install flask from pip3
 
-exec { 'flask':
-  command =>'/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
