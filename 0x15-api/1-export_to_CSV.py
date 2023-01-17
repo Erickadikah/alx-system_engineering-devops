2a680ec8050c38309a5078cd481328e67c0f0f6e
erickadikah <erickadikah2030@gmail.com>
2023-01-17 20:17:45 +0300

    task

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -19,11 +19,11 @@ if __name__ == "__main__":
             id = int(sys.argv[1])
             user_res = requests.get('{}/users/{}'.format(API, id)).json()
             todos_res = requests.get('{}/todos'.format(API)).json()
-            user_name = user_res.get('name')
+            user_name = user_res.get('username')
             todos = list(filter(lambda x: x.get('userId') == id, todos_res))
             with open('{}.csv'.format(id), 'w') as file:
                 for todo in todos:
-                    file.write('"{}", "{}", "{}","{}"\n'.format(
+                    file.write('"{}","{}","{}","{}"\n'.format(
                         id,
                         user_name,
                         todo.get('completed'), todo.get('title')))
