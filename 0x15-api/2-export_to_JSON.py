11969f534d18735b896b7dc3935c8d085465cc75
erickadikah <erickadikah2030@gmail.com>
2023-01-17 20:22:03 +0300

    error checked

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -4,10 +4,10 @@ Using https://jsonplaceholder.typicode.com
 returns info about employee TODO progress
 Implemented using recursion
 """
+import json
 import re
 import requests
 import sys
-import json
 
 
 API = "https://jsonplaceholder.typicode.com"
@@ -20,7 +20,7 @@ if __name__ == "__main__":
             id = int(sys.argv[1])
             user_res = requests.get('{}/users/{}'.format(API, id)).json()
             todos_res = requests.get('{}/todos'.format(API)).json()
-            user_name = user_res.get('name')
+            user_name = user_res.get('username')
             todos = list(filter(lambda x: x.get('userId') == id, todos_res))
             with open("{}.json".format(id), 'w') as json_file:
                 user_data = list(map(
