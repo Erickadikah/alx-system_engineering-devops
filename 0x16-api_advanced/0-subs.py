f5c4372bdae30a21fb6c623930a403a6360ea9e2
erickadikah <erickadikah2030@gmail.com>
2023-01-31 20:13:51 +0300

    API advance

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Return the number of subscribers for given subreddit
+"""
+
+import requests
+from sys import argv
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        "User-Agent": "My Reddit API Client/1.0"
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data["data"]["subscribers"]
+        return subscribers
+    else:
+        return 0
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Returns titles of all hot articles for a given subreddit"""
+
+import requests
+
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """Return titles of all hot articles recursive function way a subreddit"""
+    url = 'https://api.reddit.com'
+    headers = {
+        "User-Agent": "ChangeMeClient/0.1"
+    }
+    params = {
+        "after": after,
+        "count": count
+    }
+    res = requests.get("{}/r/{}/hot".format(url, subreddit),
+            headers=headers, params=params)
+    if res.status_code == 404:
+        return None
+    data = res.json().get('data')
+    after = data.get('after')
+    count += data.get('dist')
+    children = data.get('children')
+    for child in children:
+        hot_list.append(child.get('data').get('title'))
+    
+    if after is not None:
+        recurse(subreddit, hot_list, after, count)
+
+    return hot_list
