7a07291afe08f1d452164a8e2cc1e5d3aa1ee4c7
erickadikah <erickadikah2030@gmail.com>
2023-01-31 20:22:31 +0300

    compliant

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,18 +3,20 @@
 """
 import requests
 
+
 def top_ten(subreddit):
     """Print the title of the 10 hotest posts on a given subtreddit."""
     url = 'https://api.reddit.com'
     headers = {
         "User-Agent": "My Reddit API Client/1.0"
-        }
+    }
     params = {
-            "limit": 10
-            }
+        "limit": 10
+    }
 
-    res = requests.get("{}/r/{}/hot".format(url, subreddit), headers=headers, params=params)
+    res = requests.get("{}/r/{}/hot".format(url, subreddit),
+                       headers=headers, params=params)
     if res.status_code == 404:
-       return print(None)
+        return print(None)
     children = res.json().get('data').get('children')
     print("\n".join([child.get('data').get('title') for child in children]))
