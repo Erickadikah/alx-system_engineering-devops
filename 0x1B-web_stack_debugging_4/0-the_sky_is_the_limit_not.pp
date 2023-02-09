d24bfd1a9b6d44087da96aeda23c1449d1fb3424
erickadikah <erickadikah2030@gmail.com>
2023-02-10 01:00:39 +0300

    puupet lint fixed

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # changing limit ofopen files in nginx config
 exec {'change limit':
-    command => "sudo sed -i 's/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/g' /etc/default/nginx",
+    command  => "sudo sed -i 's/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/g' /etc/default/nginx",
     provider => shell
 }
 
