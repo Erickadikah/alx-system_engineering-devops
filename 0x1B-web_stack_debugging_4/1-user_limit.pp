93c4aba158ed80316b7991c9d3fb2ea07b145b8c
erickadikah <erickadikah2030@gmail.com>
2023-02-09 21:29:09 +0300

    changing os limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# changing the limit of open files in the operating system
+exec {'change limit soft':
+  command  => "sudo sed -i 's/holberton soft nofile 4/holberton soft nofile 88888/g'  /etc/security/limits.conf",
+  provider => shell
+}
+
+# change the limit of open files in  os
+exec {'change limit hard':
+  command  => "sudo sed -i 's/holberton hard nofile 5/holberton hard nofile 88888/g'  /etc/security/limits.conf",
+  provider => shell
+}
