1fa02b82fdfd7d02ff195acf86ac0d21bdd65428
erickadikah <erickadikah2030@gmail.com>
2023-02-09 21:18:41 +0300

    limit surpassed

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,28 +1,11 @@
-# Increases the amount of traffic an Nginx server can handle.
-
-# Increases the amount of traffic an Nginx server can handle.
-$file_to_edit = '/etc/nginx/nginx.conf'
-
-
-exec { 'change_line_in_file':
-  command => "sed -i '2s/4/auto/' ${file_to_edit}",
-  path    => ['/bin','/usr/bin'],
-  notify  => Exec['nginx_restart']
+# changing limit ofopen files in nginx config
+exec {'change limit':
+    command => "sudo sed -i 's/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/g' /etc/default/nginx",
+    provider => shell
 }
 
-
-
-$file_to_edit_ = '/etc/default/nginx'
-
-
-exec { 'change_line_in':
-  command => "sed -i '5s/15/4096/' ${file_to_edit_}",
-  path    => ['/bin','/usr/bin'],
-  notify  => Exec['nginx_restart']
-}
-
-exec { 'nginx_restart':
-  command     => 'service nginx restart',
-  path        => '/usr/bin:/usr/sbin:/bin:/sbin',
-  refreshonly => true,
+# restart service
+exec {'restart service':
+  command  => 'sudo service nginx restart',
+  provider => shell
 }
